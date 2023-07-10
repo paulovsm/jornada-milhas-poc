69901b38cd9afe03eb6ed237baa00574651a40c5
paulovsm <paulovsm@gmail.com>
2023-07-10 21:12:14 +0000

    aider: Add an optional `id` property to the `Destino` interface and modify the `create` method to generate a new UUID and return the created item.
    # Context:
    USER: Add an ID property to Destino entity, being this field optional, type string (UUID). The create method should set a new UUID before salving a new item and should return the created item.
    ASSISTANT: To add an ID property to the Destino entity and modify the create method, we need to modify the following files:
    
    1. `jornada-milhas-app/src/destinos/destino.interface.ts` - to add the ID property to the Destino interface.
    2. `jornada-milhas-app/src/destinos/destinos.service.ts` - to modify the create method to generate a new UUID and return the created item.
    
    Please confirm that these files are *read-write* so I can propose the necessary changes.
    USER: yes
    ASSISTANT: Great! Here are the changes you need to make:
    
    1. Add an optional `id` property to the `Destino` interface in `destino.interface.ts`:
    
    ```typescript
    jornada-milhas-app/src/destinos/destino.interface.ts
    <<<<<<< ORIGINAL
    export interface Destino {
      nome: string;
      imagemUrl: string;
    }
    =======
    export interface Destino {
      id?: string;
      nome: string;
      imagemUrl: string;
    }
    >>>>>>> UPDATED
    ```
    
    2. Modify the `create` method in `destinos.service.ts` to generate a new UUID for each new `Destino` and return the created item:
    
    ```typescript
    jornada-milhas-app/src/destinos/destinos.service.ts
    <<<<<<< ORIGINAL
    import { Injectable } from '@nestjs/common';
    import { Destino } from './destino.interface';
    
    @Injectable()
    export class DestinosService {
      private readonly destinos: Destino[] = [];
    
      create(destino: Destino) {
        this.destinos.push(destino);
      }
    
      findAll(): Destino[] {
        return this.destinos;
      }
    }
    =======
    import { Injectable } from '@nestjs/common';
    import { Destino } from './destino.interface';
    import { v4 as uuidv4 } from 'uuid';
    
    @Injectable()
    export class DestinosService {
      private readonly destinos: Destino[] = [];
    
      create(destino: Destino): Destino {
        const id = uuidv4();
        const newDestino = { id, ...destino };
        this.destinos.push(newDestino);
        return newDestino;
      }
    
      findAll(): Destino[] {
        return this.destinos;
      }
    }
    >>>>>>> UPDATED
    ```
    
    In the updated `create` method, we're using the `uuid` library to generate a new UUID, which we assign to the `id` property of the new `Destino`. We then return the new `Destino` after adding it to the `destinos` array.

--- jornada-milhas-app/src/destinos/destinos.service.ts
+++ jornada-milhas-app/src/destinos/destinos.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Destino } from './destino.interface';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class DestinosService {
   private readonly destinos: Destino[] = [];
 
-  create(destino: Destino) {
-    this.destinos.push(destino);
+  create(destino: Destino): Destino {
+    const id = uuidv4();
+    const newDestino = { id, ...destino };
+    this.destinos.push(newDestino);
+    return newDestino;
   }
 
   findAll(): Destino[] {
     return this.destinos;
   }
-
+}
   update(id: number, destino: Destino) {
     this.destinos[id] = destino;
   }
