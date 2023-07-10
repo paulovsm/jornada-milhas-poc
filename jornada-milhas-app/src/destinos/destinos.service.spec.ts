df5840adfe317848dd893620b9b426b7772e9231
paulovsm <paulovsm@gmail.com>
2023-07-10 21:49:08 +0000

    aider: Created test cases for `createDestino`, `findAll`, `updateDestino`, and `deleteDestino` methods in `destinos.service.ts`.
    # Context:
    USER: Using depoimentos.service.spec.ts as reference create the test cases for `jornada-milhas-app/src/destinos/destinos.service.ts`
    ASSISTANT: Sure, I can help with that. However, I need to know the methods that are present in `destinos.service.ts` to create the corresponding test cases. Could you please provide the content of `destinos.service.ts` or at least the list of methods that it contains?
    USER: I added these *read-write* files: jornada-milhas-app/src/destinos/destinos.service.ts
    ASSISTANT: Great, let's create the test cases for `destinos.service.ts` using `depoimentos.service.spec.ts` as a reference.
    
    We will create a new file `destinos.service.spec.ts` in the `src/destinos/` directory.
    
    The test cases will be for the following methods: `createDestino`, `findAll`, `updateDestino`, and `deleteDestino`.
    
    Here is the new file:
    
    ```typescript
    jornada-milhas-app/src/destinos/destinos.service.spec.ts
    <<<<<<< ORIGINAL
    
    =======
    import { Test, TestingModule } from '@nestjs/testing';
    import { DestinosService } from './destinos.service';
    import { Destino } from './destino.interface';
    
    describe('DestinosService', () => {
      let service: DestinosService;
    
      beforeEach(async () => {
        const module: TestingModule = await Test.createTestingModule({
          providers: [DestinosService],
        }).compile();
    
        service = module.get<DestinosService>(DestinosService);
      });
    
      it('should be defined', () => {
        expect(service).toBeDefined();
      });
    
      it('should return an array of destinos', () => {
        const result: Destino[] = [];
        jest.spyOn(service, 'findAll').mockImplementation(() => result);
        expect(service.findAll()).toBe(result);
      });
    
      it('should add a destino', () => {
        const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
        const destinoCriado = service.createDestino(destino);
        expect(service.findAll()).toContain(destinoCriado);
      });
    
      it('should update a destino', () => {
        const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
        const destinoCriado = service.createDestino(destino);
        const updatedDestino: Destino = { id: destinoCriado.id, nome: 'Updated', destino: 'Updated test', foto: 'updated.jpg' };
        service.updateDestino(updatedDestino.id, updatedDestino);
        expect(service.findAll()).toContain(updatedDestino);
      });
    
      it('should delete a destino', () => {
        const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
        const destinoCriado = service.createDestino(destino);
        service.deleteDestino(destinoCriado.id);
        expect(service.findAll()).not.toContain(destino);
      });
    
    });
    >>>>>>> UPDATED
    ```
    
    Please note that you might need to adjust the `Destino` object structure and the parameters passed to the methods according to your actual implementation.

---
+++ jornada-milhas-app/src/destinos/destinos.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DestinosService } from './destinos.service';
+import { Destino } from './destino.interface';
+
+describe('DestinosService', () => {
+  let service: DestinosService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [DestinosService],
+    }).compile();
+
+    service = module.get<DestinosService>(DestinosService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of destinos', () => {
+    const result: Destino[] = [];
+    jest.spyOn(service, 'findAll').mockImplementation(() => result);
+    expect(service.findAll()).toBe(result);
+  });
+
+  it('should add a destino', () => {
+    const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
+    const destinoCriado = service.createDestino(destino);
+    expect(service.findAll()).toContain(destinoCriado);
+  });
+
+  it('should update a destino', () => {
+    const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
+    const destinoCriado = service.createDestino(destino);
+    const updatedDestino: Destino = { id: destinoCriado.id, nome: 'Updated', destino: 'Updated test', foto: 'updated.jpg' };
+    service.updateDestino(updatedDestino.id, updatedDestino);
+    expect(service.findAll()).toContain(updatedDestino);
+  });
+
+  it('should delete a destino', () => {
+    const destino: Destino = { nome: 'Test', destino: 'This is a test', foto: 'test.jpg' };
+    const destinoCriado = service.createDestino(destino);
+    service.deleteDestino(destinoCriado.id);
+    expect(service.findAll()).not.toContain(destino);
+  });
+
+});
