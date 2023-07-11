bf87f8223125c87b001ee94243406f639fc32075
paulovsm <paulovsm@gmail.com>
2023-07-11 17:35:40 +0000

    Inclusão de novas propriedades na entidade Destino

--- jornada-milhas-app/src/destinos/destino.interface.ts
+++ jornada-milhas-app/src/destinos/destino.interface.ts
@@ -1,19 +1,30 @@
-import { IsOptional, IsUUID, IsUrl, IsNotEmpty, MinLength, IsNumber } from 'class-validator';
+import { IsOptional, IsUUID, IsUrl, IsNotEmpty, MinLength, IsNumber, MaxLength } from 'class-validator';
 
 export class Destino {
   @IsOptional()
-  @IsUUID()
+  @IsUUID(undefined, { message: 'O id deve ser um UUID válido' })
   id?: string;
 
-  @IsNotEmpty()
-  @IsUrl()
-  foto: string;
+  @IsNotEmpty({ message: 'A foto1 não pode ser vazia' })
+  @IsUrl(undefined, { context: { message: 'A foto deve ser uma URL válida' } })
+  foto1: string;
 
-  @IsNotEmpty()
-  @MinLength(3)
+  @IsNotEmpty({ message: 'A foto2 não pode ser vazia' })
+  @IsUrl(undefined, { context: { message: 'A foto deve ser uma URL válida' } })
+  foto2: string;
+
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @MinLength(3, { message: 'O nome deve ter no mínimo 3 caracteres' })
   nome: string;
 
-  @IsNotEmpty()
-  @IsNumber()
+  @IsNotEmpty({ message: 'O preco não pode ser vazio' })
+  @IsNumber({}, { message: 'O preco deve ser um número' })
   preco: number;
+
+  @IsNotEmpty({ message: 'A descricao da meta não pode ser vazia' })
+  @MaxLength(160, { message: 'A descricao da meta deve ter no máximo 160 caracteres' })
+  meta: string;
+
+  @IsOptional()
+  textoDescritivo: string;
 }
