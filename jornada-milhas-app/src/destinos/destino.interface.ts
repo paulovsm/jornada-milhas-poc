7b83cebb95bc29b688dea2e2aea0a10aeb1c0548
paulovsm <paulovsm@gmail.com>
2023-07-11 13:00:12 +0000

    aider: Apply class-validator decorators to the properties of the Destino class.
    # Context:
    USER: using class-validator library, update `jornada-milhas-app/src/destinos/destino.interface.ts` so id is optional and an UUID, foto is mandatory and an URL, name is mandatory and with 3 characters minimum, preco is mandatory and a number
    ASSISTANT: To make these changes, I need you to make the file `jornada-milhas-app/src/destinos/destino.interface.ts` *read-write*. Can you do that?
    USER: yes
    ASSISTANT: Great! Here are the changes you need to make:
    
    1. Import the necessary decorators from the `class-validator` library.
    2. Apply the decorators to the properties of the `Destino` interface.
    
    Here is the *edit block*:
    
    ```typescript
    jornada-milhas-app/src/destinos/destino.interface.ts
    <<<<<<< ORIGINAL
    export interface Destino {
      id: string;
      foto: string;
      name: string;
      preco: number;
    }
    =======
    import { IsOptional, IsUUID, IsUrl, IsNotEmpty, MinLength, IsNumber } from 'class-validator';
    
    export class Destino {
      @IsOptional()
      @IsUUID()
      id: string;
    
      @IsNotEmpty()
      @IsUrl()
      foto: string;
    
      @IsNotEmpty()
      @MinLength(3)
      name: string;
    
      @IsNotEmpty()
      @IsNumber()
      preco: number;
    }
    >>>>>>> UPDATED
    ```
    
    Please note that I've changed `Destino` from an interface to a class. This is because decorators (like `@IsOptional()`, `@IsUUID()`, etc.) can only be used with classes, not interfaces.

--- jornada-milhas-app/src/destinos/destino.interface.ts
+++ jornada-milhas-app/src/destinos/destino.interface.ts
@@ -1,6 +1,19 @@
-export interface Destino {
-  id?: string;
+import { IsOptional, IsUUID, IsUrl, IsNotEmpty, MinLength, IsNumber } from 'class-validator';
+
+export class Destino {
+  @IsOptional()
+  @IsUUID()
+  id: string;
+
+  @IsNotEmpty()
+  @IsUrl()
   foto: string;
-  nome: string;
+
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   preco: number;
 }
